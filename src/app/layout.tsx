b183298cacd07416e29a0c38b244fb3fbd9dcdc3
Byeongmin Lee <bmlee531@gmail.com>
2023-12-11 00:52:48 +0900

    update: 영문 font 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+import { Montserrat } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'YouBlog',
   description: 'Turn youtube into blog post',
 };
 
+const montserrat = Montserrat({
+  subsets: ['latin-ext'],
+  display: 'swap',
+  variable: '--font-montserrat',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={`${montserrat.variable}`}>
       <link rel="manifest" href="/pwa/manifest.json" />
       <body>{children}</body>
     </html>

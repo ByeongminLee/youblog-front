7d823bcb1d1f27de69b5afeeaa26a85fa8b3fe24
Byeongmin Lee <bmlee531@gmail.com>
2023-12-14 02:18:31 +0900

    fix: splash

--- src/components/view/SplashView.tsx
+++ src/components/view/SplashView.tsx
@@ -26,14 +26,14 @@ export const SplashView = () => {
   }
 
   return (
-    <div className={tw('w-full h-full min-w-screen min-h-dscreen', 'fixed top-0 left-0', 'bg-black', 'z-[9999999]')}>
+    <div className={tw('w-full h-full min-w-screen min-h-screen', 'fixed top-0 left-0', 'bg-black50', 'z-[100000]')}>
       <motion.div
         initial="initial"
         animate="animate"
         variants={motionVariants}
         className={tw('w-[130px]', 'absolute top-1/3 left-[calc(50%-65px)] transform -translate-y-1/2')}
       >
-        <Icon name="logo" className={tw('w-[130px] text-white')} />
+        <Icon name="logo" className={tw('w-[130px] desktop:w-[280px] text-white')} />
       </motion.div>
     </div>
   );

25e4b60f7faadc495aaf8fd6ea04db29a72d01a6
Byeongmin Lee <bmlee531@gmail.com>
2023-12-17 13:53:02 +0900

    update: url key 체크

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,14 @@ export default function Page() {
   const [inputValue, handleChangeInput, inputReset] = useInput({ youtubeUrl: '' });
 
   const handler = () => {
-    if (!inputValue.youtubeUrl.includes('www.youtube.com')) return;
+    if (!inputValue.youtubeUrl.includes('youtu')) return;
 
-    const getYoutubeKey = (url: string) => {
-      return url.replace('https://www.youtube.com/watch?v=', '');
+    const getYoutubeKey = (url: string): string | null => {
+      const match1 = url.match(/(?:https:\/\/www\.youtube\.com\/watch\?v=)([a-zA-Z0-9_-]+)/);
+
+      const match2 = url.match(/(?:https:\/\/youtu\.be\/)([^?]+)/);
+
+      return (match1 && match1[1]) || (match2 && match2[1]) || null;
     };
 
     router.push('/article?key=' + getYoutubeKey(inputValue.youtubeUrl));

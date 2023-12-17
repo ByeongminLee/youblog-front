17b3ac3b08ce59c1c4ffadbd62457df6dcda20ef
Byeongmin Lee <bmlee531@gmail.com>
2023-12-17 13:57:16 +0900

    update: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import type { Metadata } from 'next';
 import '@/styles/globals.css';
 import { Montserrat } from 'next/font/google';
 import { Providers } from '@/components/common';
-
-// export const metadata: Metadata = {
-//   title: 'YouBlog',
-//   description: 'Turn youtube into blog post',
-// };
 
 const montserrat = Montserrat({
   subsets: ['latin-ext'],
@@ -19,6 +13,9 @@ const montserrat = Montserrat({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" className={`${montserrat.variable}`}>
+      <title>youblog</title>
+      <meta name="keywords" content="ai, post" />
+      <meta name="description" content="인공지능으로 블로그 포스팅 만들기" />
       <link rel="manifest" href="/pwa/manifest.json" />
       <body>
         <Providers>{children}</Providers>
